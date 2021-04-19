82d3b6e5a2eac42656998f12949363e68326e1c8
Bo Bramer <bramer.bo@gmail.com>
2021-04-18 21:28:07 -0700

    Began working on a pendulum simulation

---
+++ src/com/bramerlabs/physics/pendulum/Main.java
@@ -0,0 +1,104 @@
+package com.bramerlabs.physics.pendulum;
+
+import com.bramerlabs.physics.soft_bodies.Spring;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+
+    private final static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private final static int width = screenSize.width/2, height = screenSize.height/2;
+    private boolean done = false, nextFrame = false;
+    private int mouseX, mouseY;
+    boolean mouseDown = false;
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    public void run() {
+        // set up objects
+
+        JFrame frame = new JFrame();
+        frame.setSize(new Dimension(width, height));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel() {
+            @Override
+            public void paint(Graphics g) {
+                super.paint(g);
+            }
+        };
+        panel.setPreferredSize(new Dimension(width, height));
+        MouseListener mouseListener = new MouseListener() {
+            public void mouseClicked(MouseEvent mouseEvent) {}
+            public void mousePressed(MouseEvent mouseEvent) {
+                mouseDown = true;
+            }
+            public void mouseReleased(MouseEvent mouseEvent) {
+                mouseDown = false;
+            }
+            public void mouseEntered(MouseEvent mouseEvent) {}
+            public void mouseExited(MouseEvent mouseEvent) {}
+        };
+        MouseMotionListener mouseMotionListener = new MouseMotionListener() {
+            public void mouseDragged(MouseEvent mouseEvent) {
+                mouseX = mouseEvent.getX();
+                mouseY = mouseEvent.getY();
+            }
+            public void mouseMoved(MouseEvent mouseEvent) {
+                mouseX = mouseEvent.getX();
+                mouseY = mouseEvent.getY();
+            }
+        };
+        KeyListener keyListener = new KeyListener() {
+            public void keyTyped(KeyEvent keyEvent) {}
+            public void keyPressed(KeyEvent keyEvent) {
+                if (keyEvent.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    done = true;
+                } else if (keyEvent.getKeyCode() == KeyEvent.VK_SPACE) {
+                    nextFrame = true;
+                }
+            }
+            public void keyReleased(KeyEvent keyEvent) {}
+        };
+        panel.addMouseListener(mouseListener);
+        panel.addMouseMotionListener(mouseMotionListener);
+        frame.addKeyListener(keyListener);
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+        panel.repaint();
+        frame.move(screenSize.width/4, screenSize.height/4);
+
+        // main application loop
+        while (!done) {
+
+            panel.repaint();
+
+
+//            // update manually
+//            while (!nextFrame) {
+//                try {
+//                    Thread.sleep(20);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            nextFrame = false;
+
+            // update automatically
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        frame.dispose();
+
+    }
+
+}
---
+++ src/com/bramerlabs/physics/pendulum/MassPoint.java
@@ -0,0 +1,9 @@
+package com.bramerlabs.physics.pendulum;
+
+import com.bramerlabs.engine.math.vector.Vector2f;
+
+public class MassPoint {
+
+    public Vector2f position, velocity, force;
+
+}
