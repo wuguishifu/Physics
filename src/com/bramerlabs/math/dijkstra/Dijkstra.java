437a2b18bb7f5003d99882b334ece6e418a2f3ae
Bo Bramer <bramer.bo@gmail.com>
2021-04-27 12:01:08 -0700

    Added functions for dragging line barriers and square barriers.

--- src/com/bramerlabs/math/dijkstra/Dijkstra.java
+++ src/com/bramerlabs/math/dijkstra/Dijkstra.java
@@ -1,20 +1,23 @@
 package com.bramerlabs.math.dijkstra;
 
+import com.bramerlabs.engine.math.vector.Vector2f;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class Dijkstra {
 
-    protected final static int CELL_SIZE = 40;
+    protected final static int CELL_SIZE = 30;
     protected final static int CELLS_HORIZONTAL = 40;
     protected final static int CELLS_VERTICAL = 30;
     protected final static int WIDTH = CELLS_HORIZONTAL * CELL_SIZE;
     protected final static int HEIGHT = CELLS_VERTICAL * CELL_SIZE;
 
-    private int mouseX, mouseY;
+    private Vector2f initialMousePosition, currentMousePosition;
 
     private int keyDown = -1;
+    private int buttonDown = -1;
 
     private boolean done = false, nextFrame = false, mouseDown = false;
 
@@ -27,6 +30,8 @@ public class Dijkstra {
     private void run() {
         // set up objects
         grid = new Grid(CELLS_HORIZONTAL, CELLS_VERTICAL, CELL_SIZE);
+        initialMousePosition = Vector2f.zero;
+        currentMousePosition = Vector2f.zero;
 
         JFrame frame = new JFrame();
         frame.setSize(new Dimension(WIDTH, HEIGHT));
@@ -36,6 +41,7 @@ public class Dijkstra {
             public void paint(Graphics g) {
                 super.paint(g);
                 grid.paint(g);
+//                g.drawLine((int) initialMousePosition.x, (int) initialMousePosition.y, (int) currentMousePosition.x, (int) currentMousePosition.y);
             }
         };
 
@@ -43,8 +49,10 @@ public class Dijkstra {
         MouseListener mouseListener = new MouseListener() {
             public void mouseClicked(MouseEvent mouseEvent) {}
             public void mousePressed(MouseEvent mouseEvent) {
+                buttonDown = mouseEvent.getButton();
                 switch (mouseEvent.getButton()) {
                     case MouseEvent.BUTTON1:
+                        initialMousePosition = new Vector2f(mouseEvent.getX(), mouseEvent.getY());
                         switch (keyDown) {
                             case -1:
                                 grid.setCell(mouseEvent.getX(), mouseEvent.getY(), 1);
@@ -62,12 +70,30 @@ public class Dijkstra {
                         break;
                 }
             }
-            public void mouseReleased(MouseEvent mouseEvent) {}
+            public void mouseReleased(MouseEvent mouseEvent) {
+                buttonDown = -1;
+                initialMousePosition = Vector2f.zero;
+                grid.copyTempCells();
+            }
             public void mouseEntered(MouseEvent mouseEvent) {}
             public void mouseExited(MouseEvent mouseEvent) {}
         };
         MouseMotionListener mouseMotionListener = new MouseMotionListener() {
-            public void mouseDragged(MouseEvent mouseEvent) {}
+            public void mouseDragged(MouseEvent mouseEvent) {
+                currentMousePosition = new Vector2f(mouseEvent.getX(), mouseEvent.getY());
+                if (buttonDown == MouseEvent.BUTTON1) {
+                    if ((int) currentMousePosition.x / CELL_SIZE != (int) initialMousePosition.x / CELL_SIZE || (int) currentMousePosition.y / CELL_SIZE != (int) initialMousePosition.y / CELL_SIZE) {
+                        if (keyDown == KeyEvent.VK_B) {
+                            grid.setTempCellsSquare(initialMousePosition, currentMousePosition);
+                        } else {
+                            grid.setTempCells(initialMousePosition, currentMousePosition, true);
+                        }
+                    }
+                }
+                if (buttonDown == MouseEvent.BUTTON3) {
+                    grid.setCell(mouseEvent.getX(), mouseEvent.getY(), -1);
+                }
+            }
             public void mouseMoved(MouseEvent mouseEvent) {}
         };
         KeyListener keyListener = new KeyListener() {
--- src/com/bramerlabs/math/dijkstra/Grid.java
+++ src/com/bramerlabs/math/dijkstra/Grid.java
@@ -1,5 +1,7 @@
 package com.bramerlabs.math.dijkstra;
 
+import com.bramerlabs.engine.math.vector.Vector2f;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
@@ -7,19 +9,30 @@ public class Grid {
 
     public int cellsHorizontal, cellsVertical, cellSize;
     public int[][] cells;
+    public int[][] tempCells;
 
     public boolean containsStart = false, containsEnd = false;
     public int[] start, end;
+
+    public final float stepSize;
 
     public Grid(int cellsHorizontal, int cellsVertical, int cellSize) {
         this.cellsHorizontal = cellsHorizontal;
         this.cellsVertical = cellsVertical;
         this.cellSize = cellSize;
+        this.stepSize = 0.1f;
 
         this.cells = new int[cellsHorizontal][cellsVertical];
         for (int i = 0; i < cellsHorizontal; i++) {
             for (int j = 0; j < cellsVertical; j++) {
                 cells[i][j] = -1;
+            }
+        }
+
+        this.tempCells = new int[cellsHorizontal][cellsVertical];
+        for (int i = 0; i < cellsHorizontal; i++) {
+            for (int j = 0; j < cellsVertical; j++) {
+                tempCells[i][j] = -1;
             }
         }
 
@@ -31,11 +44,70 @@ public class Grid {
         if (!(containsEnd && containsStart)) {
             return;
         }
-        System.out.println("(" + start[0] + ", " + start[1] + ")   (" + end[0] + ", " + end[1] + ")");
+
+        // do the algorithm here
+
+    }
+
+    public void setTempCellsSquare(Vector2f p1, Vector2f p2) {
+        Vector2f p12 = new Vector2f(p1.x, p2.y);
+        Vector2f p21 = new Vector2f(p2.x, p1.y);
+        clearTempCells();
+        setTempCells(p1, p21, false);
+        setTempCells(p1, p12, false);
+        setTempCells(p2, p21, false);
+        setTempCells(p2, p12, false);
+    }
+
+    public void setTempCells(Vector2f p1, Vector2f p2, boolean clear) {
+        p1.x = (float) (cellSize * (int) (p1.x / cellSize)) + cellSize / 2f;
+        p1.y = (float) (cellSize * (int) (p1.y / cellSize)) + cellSize / 2f;
+        p2.x = (float) (cellSize * (int) (p2.x / cellSize)) + cellSize / 2f;
+        p2.y = (float) (cellSize * (int) (p2.y / cellSize)) + cellSize / 2f;
+        if (clear) {
+            clearTempCells();
+        }
+        Vector2f incidentVector = p1;
+        Vector2f step = Vector2f.normalize(Vector2f.subtract(p2, p1), stepSize);
+        float maxDistance = Vector2f.distance(p1, p2);
+        float currentDistance = 0;
+        while (currentDistance < maxDistance) {
+            currentDistance += stepSize;
+            incidentVector = Vector2f.add(incidentVector, step);
+            setTempCell(incidentVector.x, incidentVector.y);
+        }
+    }
+
+    public void copyTempCells() {
+        for (int i = 0; i < cellsHorizontal; i++) {
+            for (int j = 0; j < cellsVertical; j++) {
+                if (tempCells[i][j] == 1) {
+                    setCell(i * cellSize, j * cellSize, 1);
+                }
+            }
+        }
+        for (int i = 0; i < cellsHorizontal; i++) {
+            for (int j = 0; j < cellsVertical; j++) {
+                tempCells[i][j] = -1;
+            }
+        }
+    }
+
+    public void setTempCell(float x, float y) {
+        if (x < 0 || x > Dijkstra.WIDTH || y < 0 || y > Dijkstra.HEIGHT) {
+            return;
+        }
+        tempCells[(int) (x / cellSize)][(int) (y / cellSize)] = 1;
     }
 
     public void setCell(float x, float y, int value) {
-        if (!(value == 1 || value == -1)) {
+        if (x < 0 || x > Dijkstra.WIDTH || y < 0 || y > Dijkstra.HEIGHT) {
+            return; // check bounds
+        }
+        if (cells[(int) (x / cellSize)][(int) (y / cellSize)] == value) {
+            return; // optimization
+        }
+        if (value == 2 || value == 3) {
             clearCells(value);
         }
         if (value == 2) {
@@ -74,13 +146,19 @@ public class Grid {
         }
     }
 
+    public void clearTempCells() {
+        for (int i = 0; i < cellsHorizontal; i++) {
+            for (int j = 0; j < cellsVertical; j++) {
+                tempCells[i][j] = -1;
+            }
+        }
+    }
+
     public void paint(Graphics g) {
         // draw the cells
         for (int i = 0; i < cellsHorizontal; i++) {
             for (int j = 0; j < cellsVertical; j++) {
                 switch (cells[i][j]) {
-                    case -1:
-                        break;
                     case 1:
                         g.setColor(Color.DARK_GRAY);
                         g.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
@@ -93,6 +171,15 @@ public class Grid {
                         g.setColor(Color.RED);
                         g.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
                         break;
+                }
+            }
+        }
+
+        for (int i = 0; i < cellsHorizontal; i++) {
+            for (int j = 0; j < cellsVertical; j++) {
+                if (tempCells[i][j] == 1) {
+                    g.setColor(Color.LIGHT_GRAY);
+                    g.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
                 }
             }
         }
