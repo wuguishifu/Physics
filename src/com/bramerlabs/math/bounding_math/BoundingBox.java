0c86603a017eb40ec6582c65806ff71e47f84fd5
Bo Bramer <bramer.bo@gmail.com>
2021-05-09 15:03:28 -0700

    Added code for testing different types of bounding box math

---
+++ src/com/bramerlabs/math/bounding_math/BoundingBox.java
@@ -0,0 +1,118 @@
+package com.bramerlabs.math.bounding_math;
+
+import com.bramerlabs.engine.math.vector.Vector2f;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+@SuppressWarnings({"deprecation"})
+public class BoundingBox {
+
+    protected final static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    protected final static int width = screenSize.width, height = screenSize.height;
+    private boolean done = false, nextFrame = false;
+    private int mouseX, mouseY;
+    private boolean mouseDown = false;
+
+    private static boolean fullscreen = true;
+
+    public static void main(String[] args) {
+        new BoundingBox().run();
+    }
+
+    public void run() {
+        // set up objects
+        Hexagon hexagon = new Hexagon(new Vector2f(screenSize.width/2f, screenSize.height/2f));
+
+        JFrame frame = new JFrame();
+        frame.setSize(new Dimension(width, height));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        if (fullscreen) {
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            frame.setUndecorated(true);
+            frame.setVisible(true);
+        }
+        JPanel panel = new JPanel() {
+            @Override
+            public void paint(Graphics g) {
+                super.paint(g);
+                hexagon.paint(g);
+            }
+        };
+        panel.setPreferredSize(new Dimension(width, height));
+        MouseListener mouseListener = new MouseListener() {
+            public void mouseClicked(MouseEvent mouseEvent) {}
+            public void mousePressed(MouseEvent mouseEvent) {
+                if (mouseEvent.getButton() == MouseEvent.BUTTON1) {
+                    mouseDown = true;
+                }
+            }
+            public void mouseReleased(MouseEvent mouseEvent) {
+                if (mouseEvent.getButton() == MouseEvent.BUTTON1) {
+                    mouseDown = false;
+                }
+            }
+            public void mouseEntered(MouseEvent mouseEvent) {}
+            public void mouseExited(MouseEvent mouseEvent) {}
+        };
+        MouseMotionListener mouseMotionListener = new MouseMotionListener() {
+            public void mouseDragged(MouseEvent mouseEvent) {}
+            public void mouseMoved(MouseEvent mouseEvent) {
+                mouseX = mouseEvent.getX();
+                mouseY = mouseEvent.getY();
+            }
+        };
+        KeyListener keyListener = new KeyListener() {
+            public void keyTyped(KeyEvent keyEvent) {}
+            public void keyPressed(KeyEvent keyEvent) {
+                if (keyEvent.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    done = true;
+                } else if (keyEvent.getKeyCode() == KeyEvent.VK_SPACE) {
+                    nextFrame = true;
+                }
+            }
+            public void keyReleased(KeyEvent keyEvent) {}
+        };
+        panel.addMouseListener(mouseListener);
+        panel.addMouseMotionListener(mouseMotionListener);
+        frame.addKeyListener(keyListener);
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+        panel.repaint();
+        if (!fullscreen) {
+            frame.move(screenSize.width / 4, screenSize.height / 4);
+        }
+
+        // main application loop
+        while (!done) {
+
+            hexagon.isSelected = hexagon.checkCollision(mouseX, mouseY);
+
+            panel.repaint();
+
+//            // update manually
+//            while (!nextFrame) {
+//                try {
+//                    Thread.sleep(20);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            nextFrame = false;
+
+            // update automatically
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        frame.dispose();
+
+    }
+
+}
---
+++ src/com/bramerlabs/math/bounding_math/Hexagon.java
@@ -0,0 +1,49 @@
+package com.bramerlabs.math.bounding_math;
+
+import com.bramerlabs.engine.math.vector.Vector2f;
+import com.bramerlabs.engine.math.vector.Vector3f;
+
+import java.awt.*;
+
+public class Hexagon {
+
+    protected float radius = 300;
+    protected float triangleHeight = (float) Math.sqrt(3) * 0.5f * radius;
+    protected Vector2f position;
+
+    protected static final double[] hexPointAngles = new double[]{Math.toRadians(30), Math.toRadians(90), Math.toRadians(150), Math.toRadians(210), Math.toRadians(270), Math.toRadians(330), Math.toRadians(30)};
+    protected static final double[] hexFaceAngles = new double[]{-Math.toRadians(0), -Math.toRadians(60), -Math.toRadians(120), -Math.toRadians(180), -Math.toRadians(240), -Math.toRadians(300), -Math.toRadians(0)};
+    protected static final float height = 0.2f;
+
+    protected boolean isSelected = false;
+
+    protected static Vector3f[] hexCornerPoints = new Vector3f[]{
+            new Vector3f((float) Math.cos(hexPointAngles[6]), 0, (float) Math.sin(hexPointAngles[6])),
+            new Vector3f((float) Math.cos(hexPointAngles[5]), 0, (float) Math.sin(hexPointAngles[5])),
+            new Vector3f((float) Math.cos(hexPointAngles[4]), 0, (float) Math.sin(hexPointAngles[4])),
+            new Vector3f((float) Math.cos(hexPointAngles[3]), 0, (float) Math.sin(hexPointAngles[3])),
+            new Vector3f((float) Math.cos(hexPointAngles[2]), 0, (float) Math.sin(hexPointAngles[2])),
+            new Vector3f((float) Math.cos(hexPointAngles[1]), 0, (float) Math.sin(hexPointAngles[1])),
+            new Vector3f((float) Math.cos(hexPointAngles[0]), 0, (float) Math.sin(hexPointAngles[0])),
+    };
+
+    public Hexagon(Vector2f position) {
+        this.position = position;
+    }
+
+    public boolean checkCollision(float mouseX, float mouseY) {
+        Vector2f mouseLoc = new Vector2f(mouseX, mouseY);
+        return !(Vector2f.distance(mouseLoc, position) < triangleHeight);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(isSelected ? Color.BLUE : Color.RED);
+        for (int i = 0; i < hexCornerPoints.length - 1; i++) {
+            Vector2f point1 = Vector2f.add(Vector2f.normalize(new Vector2f(hexCornerPoints[i].x, hexCornerPoints[i].z), radius), position);
+            Vector2f point2 = Vector2f.add(Vector2f.normalize(new Vector2f(hexCornerPoints[i + 1].x, hexCornerPoints[i + 1].z), radius), position);
+            g.drawLine((int) point1.x, (int) point1.y, (int) point2.x, (int) point2.y);
+        }
+        g.drawOval((int) (position.x - triangleHeight), (int) (position.y - triangleHeight), (int) triangleHeight * 2, (int) triangleHeight * 2);
+    }
+
+}
