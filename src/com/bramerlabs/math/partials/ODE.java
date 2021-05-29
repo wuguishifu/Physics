f14463c9e4d8ac1884efb05443f34602de213ac8
Bo Bramer <bramer.bo@gmail.com>
2021-05-28 19:24:59 -0700

    Added boundary condition function

--- src/com/bramerlabs/math/partials/ODE.java
+++ src/com/bramerlabs/math/partials/ODE.java
@@ -1,7 +1,14 @@
 package com.bramerlabs.math.partials;
 
 public class ODE {
+    private float[] bounds;
+
     public float function(float x, float y, float z) {
         return -1;
+    }
+
+    public float[] bounds(float xL, float xR, float yL, float yR) {
+        this.bounds = new float[]{xL, xR, yL, yR};
+        return this.bounds;
     }
 }
