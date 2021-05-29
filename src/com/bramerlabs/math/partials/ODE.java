1ad0cc83427e0324539a273bf712e9315bf0aadf
Bo Bramer <bramer.bo@gmail.com>
2021-05-28 19:25:38 -0700

    Added initial condition function

--- src/com/bramerlabs/math/partials/ODE.java
+++ src/com/bramerlabs/math/partials/ODE.java
@@ -2,6 +2,7 @@ package com.bramerlabs.math.partials;
 
 public class ODE {
     private float[] bounds;
+    private float[] ic;
 
     public float function(float x, float y, float z) {
         return -1;
@@ -10,5 +11,10 @@ public class ODE {
     public float[] bounds(float xL, float xR, float yL, float yR) {
         this.bounds = new float[]{xL, xR, yL, yR};
         return this.bounds;
+    }
+
+    public float[] ic(float x0, float y0) {
+        this.ic = new float[]{x0, y0};
+        return this.ic;
     }
 }
