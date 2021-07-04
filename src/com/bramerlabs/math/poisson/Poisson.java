73d88cde4f14dfd0516a7fcb92f787b4858ae944
Bo Bramer <bramer.bo@gmail.com>
2021-07-03 22:41:40 -0700

    Began implementing Robert Bridson's fast poisson disc sampling algorithm

--- src/com/bramerlabs/math/poisson/Poisson.java
+++ src/com/bramerlabs/math/poisson/Poisson.java
@@ -10,46 +10,12 @@ public class Poisson {
                                                      int numSamplesBeforeRejection) {
 
         ArrayList<Vector2f> points = new ArrayList<>();
-        ArrayList<Vector2f> spawnPoints = new ArrayList<>();
 
-        spawnPoints.add(new Vector2f(sampleRegionSize.x/2, sampleRegionSize.y/2));
-        while (spawnPoints.size() > 0) {
-            int spawnIndex = (int) (spawnPoints.size() * Math.random());
-            Vector2f spawnCenter = spawnPoints.get(spawnIndex);
-            boolean candidateAccepted = false;
+        int sampleX = (int) Math.ceil(sampleRegionSize.x / (radius / Math.sqrt(2)));
+        int sampleY = (int) Math.ceil(sampleRegionSize.y / (radius / Math.sqrt(2)));
+        int[][] sampleArray = new int[sampleX][sampleY];
 
-            for (int i = 0; i < numSamplesBeforeRejection; i++) {
-                float angle = (float) (Math.random() * Math.PI * 2);
-                Vector2f dir = new Vector2f((float) Math.sin(angle), (float) Math.cos(angle));
-                Vector2f candidate = Vector2f.add(spawnCenter, Vector2f.scale(dir,
-                        (float) (200 * Math.random())));
-                if (isValid(candidate, sampleRegionSize, radius, points)) {
-                    points.add(candidate);
-                    spawnPoints.add(candidate);
-                    candidateAccepted = true;
-                    break;
-                }
-            }
-            if (!candidateAccepted) {
-                spawnPoints.remove(spawnIndex);
-            }
-        }
         return points;
-    }
-
-    private static boolean isValid(Vector2f candidate, Vector2f sampleRegionSize, float radius,
-                                   ArrayList<Vector2f> points) {
-        if (candidate.x >= 0 && candidate.x < sampleRegionSize.x &&
-                candidate.y >= 0 && candidate.y < sampleRegionSize.y) {
-
-            for (Vector2f test : points) {
-                if (Vector2f.distance(test, candidate) < 2 * radius) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
     }
 
 }
