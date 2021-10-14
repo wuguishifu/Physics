c575e73d5153c533ca559b60123aa25e52601207
Bo Bramer <bramer.bo@gmail.com>
2021-10-13 17:52:02 -0700

    Worked on a new algorithm to prevent particles from colliding into each other

--- src/com/bramerlabs/physics/gravity_2d/Particle.java
+++ src/com/bramerlabs/physics/gravity_2d/Particle.java
@@ -10,9 +10,8 @@ public class Particle {
     public Vector2f position;
     public Vector2f velocity = new Vector2f(0, 0);
     public Vector2f force = new Vector2f(0, 0);
-    public Vector2f friction = new Vector2f(0, 0);
 
-    public int radius = 10;
+    public int radius = 20;
 
     public Color color = new Color(45, 147, 35);
 
@@ -27,7 +26,7 @@ public class Particle {
                 continue;
             }
             float magnitude = (float) Math.pow(Vector2f.distance(particle.position, this.position), 2);
-            Vector2f normal = Vector2f.normalize(Vector2f.subtract(particle.position, this.position), 1000 * (1/magnitude));
+            Vector2f normal = Vector2f.normalize(Vector2f.subtract(particle.position, this.position), 100 * (1/magnitude));
             this.force = Vector2f.add(force, normal);
         }
 
@@ -37,7 +36,18 @@ public class Particle {
             force = Vector2f.add(force, friction);
         }
 
-        this.position = Vector2f.add(position, velocity);
+        Vector2f newPosition = Vector2f.add(position, velocity);
+        for (Particle particle : particles) {
+            if (this == particle) {
+                continue;
+            }
+            if (Vector2f.distance(particle.position, newPosition) < 2 * radius) {
+                Vector2f normal = Vector2f.normalize(Vector2f.subtract(newPosition, particle.position));
+                newPosition = Vector2f.add(newPosition, normal);
+            }
+        }
+
+        this.position = newPosition;
         this.velocity = Vector2f.add(velocity, force);
     }
 
